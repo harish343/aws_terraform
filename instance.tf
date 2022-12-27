bb3a3ccf52d899c0915a2fde6bc74e5141ee9e26
harish <harishnarnaware74@gmail.com>
2022-12-27 19:08:07 +0530

    Dynamic Block in Terraform

--- instance.tf
+++ instance.tf
@@ -11,30 +11,17 @@ resource "aws_security_group" "allow_tls" {
   name = "allow_tls"
   description = "Allow TLS inbound traffic"
 
-  ingress{
-    description = "TLS from VPS"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+     for_each = [22,80,3306,27017]
+     iterator = port
+     content {
+      description = "TLS from VPC"
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+     }  
   }
-  ingress{
-    description = "TLS from VPS"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    description = "TLS from VPS"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    # cidr_blocks = [aws_vpc.main.cidr_block]
-    cidr_blocks = ["0.0.0.0/0"]
-   
-  }
-
 }
 
 resource "aws_instance" "web" {
