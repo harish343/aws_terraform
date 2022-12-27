b76927f3a9f5013d082adf37b95d87f85c291f26
harish <harishnarnaware74@gmail.com>
2022-12-27 17:09:16 +0530

    key_pair added

---
+++ file.tf
@@ -0,0 +1,3 @@
+output "printfile" {
+  value = path.module
+}
--- instance.tf
+++ instance.tf
@@ -1,3 +1,11 @@
+
+resource "aws_key_pair" "key-tf" {
+  key_name   = "key-tf"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+output "keyname" {
+  value = "${aws_key_pair.key-tf.key_name}"
+}
 resource "aws_instance" "web" {
   ami           = "ami-0574da719dca65348"
   instance_type = "t2.micro"
@@ -5,4 +13,4 @@ resource "aws_instance" "web" {
   tags = {
     Name = "First_tf_instance"
   }
-}
+}s
